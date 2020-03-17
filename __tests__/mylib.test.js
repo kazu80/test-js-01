7a61feb4b40965ee53965286d3eabc1ec5b1c299
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2020-03-17 17:30:33 +0900

    initial commit

---
+++ __tests__/mylib.test.js
@@ -0,0 +1,11 @@
+import { add, validatePassword, validateEmail } from '../mylib';
+
+describe('plus', () => {
+});
+
+describe('validate Password', () => {
+});
+
+describe('validate Email', () => {
+});
+
---
+++ mylib.js
@@ -0,0 +1,33 @@
+/**
+ * 参考
+ * https://sbfl.net/blog/2019/01/20/javascript-unittest/
+ */
+
+/**
+ * a + bの結果を返してください
+ *
+ * aとbは数値が入力されるはずです。
+ */
+export function add(a, b) {
+}
+
+
+/**
+ * パスワードチェックを作ってください.
+ * パスワードが正常場合はtrueを異常な場合はfalseを返してください
+ *
+ * パスワードは５文字以上です
+ * パスワードは10文字未満です
+ * アルファベットと数値のみで構成させてください
+ *
+ */
+export function validatePassword (password) {
+}
+
+/**
+ * Emailのバリデーションを作ってください
+ * 正常場合はtrueを異常な場合はfalseを返してください
+ *
+ */
+export function validateEmail (email) {
+}
